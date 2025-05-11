4e4f3ab4dec5d146cf9ad3bbe66ee4424a488db8
Maksym Shylo <mrfreezediables@gmail.com>
2025-05-11 16:22:08 +0200

    Configure Stack navigation. Add ProfileScreen.

--- App.js
+++ App.js
@@ -2,28 +2,48 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import { FiltersProvider } from "./utils/filters-context";
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import {NavigationContainer} from '@react-navigation/native'
 import Ionicons from 'react-native-vector-icons/Ionicons'
+import { TouchableOpacity } from "react-native";
 
 import { HomeScreen } from "./screens/home-screen";
 import { CalendarScreen } from './screens/calendar-screen'
+import { ProfileScreen } from "./screens/profile-screen";
 
+const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} options={({ navigation }) => ({
+        headerRight: () => (
+          <TouchableOpacity onPress={() => navigation.navigate('Profile')}>
+            <Ionicons name="person-outline" size={27} color="#000" />
+          </TouchableOpacity>
+        ),
+      })}/>
+      <Stack.Screen name="Profile" component={ProfileScreen} />
+    </Stack.Navigator>
+  )
+}
 
 export default function App() {
   return (
     <FiltersProvider>
       <NavigationContainer>
-          <Tab.Navigator
+      <Tab.Navigator
             screenOptions={{
               tabBarActiveTintColor: 'black',
               tabBarInactiveTintColor: 'gray',
-              tabBarIconStyle: { width: 28, height: 28 }
+              tabBarIconStyle: { width: 28, height: 28 },
+              headerShown: false
             }}
           >
             <Tab.Screen 
               name="Home" 
-              component={HomeScreen} 
+              component={HomeStack} 
               options={{
                 tabBarIcon: ({focused, color, size}) => {
                   return <Ionicons name={focused ? 'home' : 'home-outline'} size={size} color={color}></Ionicons>
---
+++ screens/profile-screen.jsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native"
+
+export const ProfileScreen = () => {
+    return (
+        <Text>This is profile screen</Text>
+    )
+}
